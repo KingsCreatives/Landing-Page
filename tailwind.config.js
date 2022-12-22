0587f1cfdee7afc3b7c8afcc29544c5d3cda5a74
Samuel Kingsford Amoah <111431959+Kwamecody@users.noreply.github.com>
2022-12-22 10:23:25 +0000

    header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
+  content: ['./*.html'],
   theme: {
     screens:{
       sm: '480px',
       md: '768px',
       lg : '976px',
-      xl: '1440px'
+      xl: '1440px'},
 
-    },
+      
     extend: {
       colors:{
-        brightRed : 'hsl (12, 88% , 59%)',
-        brightRedLight: 'hsl(12, 88%, 69%)',
-        brightRedSupLight : 'hsl(12, 88%, 95%)',
-        darkBlue: 'hsl(288, 39%, 23%)',
-        darkGrayishBlue: 'hsl (277, 12%, 61%)',
-        veryDarkBlue: 'hsl (233, 12%, 13%)',
-        veryPaleRed: 'hsl (13, 100%, 96%)',
-        veryLightGray: 'hsl(0,0%, 98%)',
+        'brightRed' :'#f25f3a',
+        'brightRedSupLight' :'#fdece7',
+        'brightRedLight' :'#f6866a',
+        'darkBlue' :'#482452',
+        'darkGrayishBlue' :'#9e90a7',
+        'veryDarkBlue': '#1d1e25',
+        'veryPaleRed':'#ff#fafafa'
       }
-    },
+    }
+    
   },
   plugins: [],
 }
